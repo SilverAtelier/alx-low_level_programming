ea346659a4919c1171361612688b6a1d505b28d9
Al-vaati01 <alvinvaati@gmail.com>
2022-06-30 16:56:38 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
 int main(){
-	char myStr[40];
 
-	strcpy(myStr, "Programming is like building a multilingual puzzle");
-
-	puts(myStr);
+	puts("Programming is like building a multilingual puzzle");
 
 	return 0;
 }
