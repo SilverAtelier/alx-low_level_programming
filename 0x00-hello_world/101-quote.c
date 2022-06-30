f031d627f9d43dbe735681603d12e7fb29fba768
Al-vaati01 <alvinvaati@gmail.com>
2022-06-30 18:32:29 +0000

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,11 @@
 #include<stdio.h>
-int main(){
-	return 0;
+/**
+ * main - entry of program
+ * Return: always 0 is success
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
 }
