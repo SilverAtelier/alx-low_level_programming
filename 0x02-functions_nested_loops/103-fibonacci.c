31ec314ea643383a5d08c70051f169b2cfd3bc9d
Al-vaati01 <alvinvaati@gmail.com>
2022-07-05 22:35:37 +0000

    my work

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+/**
+ * main-print fibo numbers
+ * Return:0 is success
+ */
+int main(void)
+{
+	unsigned long int cur, sum, next, count, num, prev;
+
+	cur = 1;
+	prev = 0;
+	count = 1;
+	num = 4000000;
+	while (count <= num)
+	{
+		next = cur + prev;
+		printf("%d", next);
+		if ((next % 2) == 0)
+		{
+			sum += next;
+		}
+		prev = cur;
+		cur = next;
+		count++;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
