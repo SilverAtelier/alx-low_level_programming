edb394b91217483a51be51c5498964986946cfb6
Al-vaati01 <alvinvaati@gmail.com>
2022-07-05 19:11:53 +0000

    my work

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,15 +2,17 @@
 /**
  * _islower-checks for lowercase
  * Return: Always 0 is success
+ * @c:charcter input
  */
 int _islower(int c)
 {
-	int letter = c;
+	int letter;
+
+	letter = 'a';
 
 	if (letter >= 'a' && letter <= 'z')
 	{
 		return (1);
 	}
-	
 	return (0);
 }
