9b49c15df3931eed5f838a02b12fc6c4b41d6684
Al-vaati01 <alvinvaati@gmail.com>
2022-07-05 18:57:00 +0000

    my work

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,15 +5,14 @@
  */
 void print_alphabet_x10(void)
 {
-	char letter = 'a';
+	char letter;
 	int i;
 	for (i = 1; i <= 10; i++)
 	{
-		while (letter <= 'z')
+		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
-			letter++;
 		}
-	} 
-	_putchar('\n');
+		_putchar('\n');
+	}
 }
