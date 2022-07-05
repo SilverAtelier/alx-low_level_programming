85fe19584ab041818d16ca5e2cc4f80195101a4f
Al-vaati01 <alvinvaati@gmail.com>
2022-07-05 18:31:18 +0000

    my work

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,18 @@
-#include<unistd.h>
 #include"main.h"
 /**
  * main-start of program
  * Return: Always 0 is success
  */
-int main(void)
+void print_alphabet_x10(void)
 {
-	print_alphabet_x10();
+	char letter = 'a';
+	int i = 1;
+	do{
+		do{
+			_putchar(letter);
+			letter++;
+		} while (letter <= 'z');
+		i++;
+	} while (i <= 10);
 	_putchar('\n');
-	return (0);
 }
