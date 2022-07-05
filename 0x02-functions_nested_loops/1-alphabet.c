3ef2d0b7f616a74a44a3f4c6a21641dae7d0b8c7
Al-vaati01 <alvinvaati@gmail.com>
2022-07-05 18:20:43 +0000

    my work

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include"main.h"
 
 /**
- * main-start of program
+ * print_alphabet-prints alphabets
  * Return: always 0 is success
  */
 void print_alphabet(void)
@@ -11,6 +11,6 @@ void print_alphabet(void)
 	do {
 		_putchar(letter);
 		letter++;
-	}while (letter <= 'z');
+	} while (letter <= 'z');
 	_putchar('\n');
 }
