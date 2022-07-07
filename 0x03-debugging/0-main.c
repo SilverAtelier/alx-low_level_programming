3a17f23562a18ac13d38e0626aafacd3f8bc6ee7
Al-vaati01 <alvinvaati@gmail.com>
2022-07-07 10:55:38 +0000

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main-runs test
+ * Return-0 is success
+ */
+int main(void)
+{
+	int j;
+
+	i =100;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+#desciption of this directory
