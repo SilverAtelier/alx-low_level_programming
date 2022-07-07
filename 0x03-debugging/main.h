c5539e1a0f47fdba115eb5413d504e78ac9c2d9f
Al-vaati01 <alvinvaati@gmail.com>
2022-07-07 17:40:18 +0000

    my work

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,11 @@
 
 #include <stdio.h>
 
-void positive_or negative(int i)
+void positive_or negative(int i);
+
+int largest_number(int a, int b, int c);
+
+void print_remaining_days(int month, int day, int year);
+
 
 #endif /* MAIN_H */
