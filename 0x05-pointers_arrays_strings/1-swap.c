f278a390c52672f5020b8662ee79e97c8159933d
Al-vaati01 <alvinvaati@gmail.com>
2022-07-11 13:53:43 +0000

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main"
+/**
+ * swap_int-swaps the values of 2 integers
+ * @*a:var1
+ * @*a:var2
+ */
+void swap_int(int *a, int *b)
+{
+	int vala, valb;
+
+	vala = a;
+	valb = b;
+	*a = valb;
+	*b = vala;
+}
