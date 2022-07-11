254f4f133a6d5c32f093f24abcbc362d1cc27e26
Al-vaati01 <alvinvaati@gmail.com>
2022-07-11 11:33:03 +0000

    my work

---
+++ 0x05-pointers_arrays_strings/maiin.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * putchar -puts character to std output
+ */
+int putchar(char c);
+
+/**
+ * reset_to_98-updates the value it points to to 98
+ */
+void reset_to_98(int *n);
+#endif
