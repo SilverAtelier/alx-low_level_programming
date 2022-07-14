7822f60af40a0ab8a257f6d8288e91da9c4d1413
Al-vaati01 <107029662+Al-vaati01@users.noreply.github.com>
2022-07-14 18:01:57 +0300

    my work

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *strncpy-copies a string.
+ * @*dest:var 1
+ * @*src:var 2
+ * @n: int var
+ * 
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+    char *result;
+
+    result = strcpy(*dest, *src, n);
+
+    return (result);
+}
