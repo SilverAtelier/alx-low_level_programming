f50388d8825a7a12f45ecb2c46da81fa90e708c5
Al-vaati01 <107029662+Al-vaati01@users.noreply.github.com>
2022-07-15 17:45:57 +0300

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,14 +2,24 @@
 #include "main.h"
 /**
  * *_strncat-concatenates 2 strings
- * @*dest:var 1
- * @*src:var 2
+ * @dest:var 1
+ * @src:var 2
  * @n:var integer
+ * Return: pointer
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	char *result;
+	int len = 0,i;
 
-	*result = strcat(*dest, *src);
+	while (dest[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; i < n && src[i] != '\0'; i++, len++)
+	{
+		dest[len] = src[i];
+	}
+	dest[len] = '\0';
+	return (dest);
 }
