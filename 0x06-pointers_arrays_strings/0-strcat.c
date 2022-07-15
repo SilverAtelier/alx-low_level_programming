df49643e3873b4a1c1c87cdf08390cf73f6b452b
Al-vaati01 <107029662+Al-vaati01@users.noreply.github.com>
2022-07-15 12:39:22 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,9 +10,27 @@
 
 char *_strcat(char *dest, char *src)
 {
-	char *result;
+	char val1[100], val2[100];
 
-	*result = strcat(*dest, *src);
+	val1[100] = *dest;
+	val2[100] = *src;
 
-	return (*result);
+	char s1[100] = val1[];
+	char s2[100] = val2[];
+
+	int len, j;
+
+	while (s1[len] != '\0')
+	{
+		++len;
+	}
+
+	for (j = 0; s2[j] != '\0'; ++j, ++len)
+	{
+		s1[len] = s2[j];
+		_putchar(s1[len]);
+	}
+	s1[len] = '\0';
+	_putchar(s1[len]);
+
 }
