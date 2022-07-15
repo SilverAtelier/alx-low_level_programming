2fee0a2586ad6e7441bcde45b8671f2c2b8ea7c6
Al-vaati01 <107029662+Al-vaati01@users.noreply.github.com>
2022-07-15 18:30:44 +0300

    my work

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,1 +1,18 @@
 #include "main.h"
+/**
+ * reverse_array-reverses content of an array
+ * @a:var pointer to string array
+ * @n:array count
+ */
+void reverse_array(int *a, int n)
+{
+    int count = 0, rev;
+
+    n = n - 1;
+    while (count <= n)
+    {
+        rev = a[count];
+        a[count++] = a[n];
+        a[n--] = rev;
+    }
+}
