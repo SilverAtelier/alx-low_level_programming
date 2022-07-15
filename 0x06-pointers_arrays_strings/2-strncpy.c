d522fa09f3f76f624212354012941199ac69368e
Al-vaati01 <107029662+Al-vaati01@users.noreply.github.com>
2022-07-15 17:57:14 +0300

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,7 +14,7 @@ char *_strncpy(char *dest, char *src, int n)
 
     k = 0;
 
-    while (i < n && src[i] != '\0')
+    while (k < n && src[k] != '\0')
     {
         dest[k] = src[k];
         k++;
