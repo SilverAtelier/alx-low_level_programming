bd10fbcaf5d6b1c61cfdc176980627a639efef96
Alvin <alvinvaati@gmail.com>
2022-08-04 18:15:08 +0300

    print string

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,19 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * print_all - prints anything.
+ * @format: input
+ *
+ */
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+
+	while (ap != NULL)
+	{
+		if
+
+	va_start(ap,format);
+	va_end(ap);
+}
