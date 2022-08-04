9bd787249c424cc7035e74f1bca171ec465f58ba
Alvin <alvinvaati@gmail.com>
2022-08-04 15:22:58 +0300

    var func

--- 0x10-variadic_functions/variadic_function.h
+++
@@ -1,9 +0,0 @@
-#ifndef _variadic_fun_
-#define _variadic_fun_
-
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
-
-#endif
