1440e22bdac90e3c98eb75d3edde332af98a92a7
Alvin <alvinvaati@gmail.com>
2022-08-06 00:08:04 +0300

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,40 +4,40 @@
 
 /**
  * print_char - prints char
- * @valist: valist
+ * @ap: ap
  */
-void print_char(va_list valist)
+void print_char(va_list ap)
 {
-	printf("%c", va_arg(valist, int));
+	printf("%c", va_arg(ap, int));
 }
 
 /**
  * print_int - prints int
- * @valist: valist
+ * @ap: ap
  */
-void print_int(va_list valist)
+void print_int(va_list ap)
 {
-	printf("%d", va_arg(valist, int));
+	printf("%d", va_arg(ap, int));
 }
 
 /**
  * print_float - prints float
- * @valist: valist
+ * @ap: ap
  */
-void print_float(va_list valist)
+void print_float(va_list ap)
 {
-	printf("%f", va_arg(valist, double));
+	printf("%f", va_arg(ap, double));
 }
 
 /**
  * print_string - prints string
- * @valist: valist
+ * @ap: ap
  */
-void print_string(va_list valist)
+void print_string(va_list ap)
 {
 	char *s;
 
-	s = va_arg(valist, char *);
+	s = va_arg(ap, char *);
 
 	if (s == NULL)
 	{
@@ -55,7 +55,7 @@ void print_all(const char * const format, ...)
 {
 	char *separator = "";
 	int i, j = 0;
-	va_list valist;
+	va_list ap;
 
 	type choice[] = { {'c', print_char},
 			      {'i', print_int},
@@ -63,7 +63,7 @@ void print_all(const char * const format, ...)
 			      {'s', print_string},
 			      {'\0', NULL} };
 
-	va_start(valist, format);
+	va_start(ap, format);
 	while (format != NULL && format[j] != '\0')
 	{
 		i = 0;
@@ -72,13 +72,13 @@ void print_all(const char * const format, ...)
 			if (choice[i].letter == format[j])
 			{
 				printf("%s", separator);
-				choice[i].func(valist);
+				choice[i].func(ap);
 				separator = ", ";
 			}
 			i++;
 		}
 		j++;
 	}
-	va_end(valist);
+	va_end(ap);
 	printf("\n");
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,7 +6,7 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-typedef struct type
+typedef struct vartype
 	{
 		char letter;
 		void (*func)(va_list);
